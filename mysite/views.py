831048c5a3f245e4b14b398f9bdad71ea58f87b9
shenzhentdl <3676537674@qq.com>
2024-07-11 20:51:50 +0800

    新增showpost视图，用于显示正文

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post
 
@@ -8,3 +8,11 @@ def homepage(request):
     posts = Post.objects.all()
     now = datetime.now()
     return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')

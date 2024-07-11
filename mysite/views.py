7b211ef6e58d61209de00a1bf77665b6a79c92a5
shenzhentdl <3676537674@qq.com>
2024-07-11 20:10:46 +0800

    修改homepage视图，将HTML逻辑放入index.html中

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,10 @@
+from datetime import datetime
+
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Post
 
 def homepage(request):
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append(f"No.{str(count)}:{str(post)}<br>")
-        post_lists.append(f'<small>{str(post.body)}<small><br><br>')
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    return render(request, 'index.html', locals())
